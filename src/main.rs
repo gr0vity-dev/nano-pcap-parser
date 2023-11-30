7909b199b1fc445ebc5a1c54bd1e4bc6d69ab649
Gustav Schauwecker <service@simpago.com>
2023-11-30 20:08:40 +0100

    Write results in JSON format (#2)
    * Write result in JSON format
    * code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,38 @@
-extern crate chrono;
-extern crate csv;
-extern crate pnet;
+pub(crate) mod channel_buffers;
+mod packet_processor;
 
-mod channel_buffers;
-
-use channel_buffers::{Channel, ChannelBuffers, DeserializationError};
-use chrono::{TimeZone, Utc};
-use csv::Writer;
+use chrono::{DateTime, TimeZone, Utc};
+use packet_processor::{PacketProcessor, PacketStatistics};
 use pcap::Capture;
-use pnet::packet::{
-    ethernet::{EtherTypes, EthernetPacket},
-    ipv4::Ipv4Packet,
-    tcp::TcpPacket,
-    Packet,
-};
 use std::{
     env,
-    io::Write,
-    net::{SocketAddr, SocketAddrV4},
+    fs::File,
+    io::{BufWriter, Write},
 };
 
 fn main() {
     let pcap_file = get_pcap_file_from_args();
-    let mut capture = open_pcap_file(&pcap_file);
-    let mut writer = create_csv_writer("output.csv");
-    let mut statistics = PacketStatistics::new();
-    let mut channels = ChannelBuffers::new();
+    let mut capture = Capture::from_file(pcap_file).expect("Failed to open pcap file");
+    let out_file = File::create("output.json").expect("could not create output.json");
+    let mut writer = BufWriter::new(out_file);
+    let mut packet_processor = PacketProcessor::new();
 
     while let Ok(packet) = capture.next() {
-        process_packet(&packet, &mut channels, &mut writer, &mut statistics);
+        if packet_processor.should_indicate_progress() {
+            indicate_progress()
+        }
+
+        if let Some(channel) = packet_processor.add_packet(packet.data) {
+            let timestamp = get_timestamp(&packet);
+
+            while let Some(entry) = packet_processor.parse_message(&channel, timestamp) {
+                serde_json::to_writer(&mut writer, &entry).expect("could not serialize");
+                write!(writer, "\n").unwrap();
+            }
+        }
     }
 
-    statistics.print_summary();
+    print_summary(&packet_processor.statistics);
 }
 
 fn get_pcap_file_from_args() -> String {
@@ -43,154 +44,31 @@ fn get_pcap_file_from_args() -> String {
     args[1].clone()
 }
 
-fn open_pcap_file(filename: &str) -> Capture<pcap::Offline> {
-    Capture::from_file(filename).expect("Failed to open pcap file")
+fn get_timestamp(packet: &pcap::Packet) -> DateTime<Utc> {
+    Utc.timestamp_opt(
+        packet.header.ts.tv_sec,
+        packet.header.ts.tv_usec as u32 * 1000,
+    )
+    .unwrap()
 }
 
-fn create_csv_writer(filename: &str) -> Writer<std::fs::File> {
-    let mut writer = csv::Writer::from_path(filename).unwrap();
-    writer
-        .write_record(&[
-            "Frame Number",
-            "Source IP",
-            "Destination IP",
-            "Source Port",
-            "Destination Port",
-            "Timestamp",
-            "Message Content",
-        ])
-        .unwrap();
-    writer
+fn indicate_progress() {
+    print!(".");
+    let _ = std::io::stdout().flush();
 }
 
-fn process_packet(
-    packet: &pcap::Packet,
-    channels: &mut ChannelBuffers,
-    writer: &mut Writer<std::fs::File>,
-    statistics: &mut PacketStatistics,
-) {
-    if statistics.packet_count % 10000 == 0 {
-        print!(".");
-        let _ = std::io::stdout().flush();
-    }
-    statistics.packet_count += 1;
-    if let Some(ethernet) = EthernetPacket::new(packet.data) {
-        match ethernet.get_ethertype() {
-            EtherTypes::Ipv4 => {
-                process_ipv4_packet(&ethernet, packet, channels, writer, statistics)
-            }
-            _ => {}
-        }
-    }
-}
-
-fn process_ipv4_packet(
-    ethernet: &EthernetPacket,
-    packet: &pcap::Packet,
-    channels: &mut ChannelBuffers,
-    writer: &mut Writer<std::fs::File>,
-    statistics: &mut PacketStatistics,
-) {
-    if let Some(ipv4) = Ipv4Packet::new(ethernet.payload()) {
-        if let Some(tcp) = TcpPacket::new(ipv4.payload()) {
-            statistics.tcp_count += 1;
-            process_tcp_packet(&ipv4, &tcp, packet, channels, writer, statistics);
-        }
-    }
-}
-
-fn process_tcp_packet(
-    ipv4: &Ipv4Packet,
-    tcp: &TcpPacket,
-    packet: &pcap::Packet,
-    channels: &mut ChannelBuffers,
-    writer: &mut Writer<std::fs::File>,
-    statistics: &mut PacketStatistics,
-) {
-    let raw_payload = tcp.payload();
-    let channel = Channel {
-        source: SocketAddr::V4(SocketAddrV4::new(ipv4.get_source(), tcp.get_source())),
-        destination: SocketAddr::V4(SocketAddrV4::new(
-            ipv4.get_destination(),
-            tcp.get_destination(),
-        )),
-    };
-
-    channels.add_bytes(&channel, raw_payload);
-
-    loop {
-        match channels.pop_message(&channel) {
-            Ok(None) => {
-                break;
-            }
-            Ok(Some(message)) => {
-                statistics.messages_parsed_count += 1;
-                let timestamp = format_timestamp(packet.header.ts.tv_sec, packet.header.ts.tv_usec);
-                // Inline write_packet_data logic here
-                writer
-                    .write_record(&[
-                        statistics.packet_count.to_string(),
-                        channel.source.ip().to_string(),
-                        channel.destination.ip().to_string(),
-                        channel.source.port().to_string(),
-                        channel.destination.port().to_string(),
-                        timestamp.to_string(),
-                        format!("{:?}", message),
-                    ])
-                    .unwrap();
-            }
-            Err(DeserializationError::InvalidHeader) => {
-                statistics.header_deserialization_failed_count += 1
-            }
-            Err(DeserializationError::InvalidMessage) => {
-                statistics.message_deserialization_failed_count += 1
-            }
-        }
-    }
-}
-
-fn format_timestamp(ts_sec: i64, ts_usec: i64) -> String {
-    match Utc.timestamp_opt(ts_sec, ts_usec as u32 * 1000) {
-        // Use timestamp_opt
-        chrono::LocalResult::Single(timestamp) => {
-            timestamp.format("%Y-%m-%d %H:%M:%S.%f").to_string()
-        }
-        _ => "Invalid Timestamp".to_string(), // Handle invalid or ambiguous timestamps
-    }
-}
-
-struct PacketStatistics {
-    packet_count: usize,
-    messages_parsed_count: usize,
-    header_deserialization_failed_count: usize,
-    message_deserialization_failed_count: usize,
-    tcp_count: usize,
-}
-
-impl PacketStatistics {
-    fn new() -> PacketStatistics {
-        PacketStatistics {
-            packet_count: 0,
-            messages_parsed_count: 0,
-            header_deserialization_failed_count: 0,
-            message_deserialization_failed_count: 0,
-            tcp_count: 0,
-        }
-    }
-
-    fn print_summary(&self) {
-        println!();
-        println!("packet_count {}", self.packet_count);
-        println!("tcp_count {}", self.tcp_count);
-        println!("messages_parsed_count {}", self.messages_parsed_count);
-        println!(
-            "header_deserialization_failed_count {}",
-            self.header_deserialization_failed_count
-        );
-        println!(
-            "message_deserialization_failed_count {}",
-            self.message_deserialization_failed_count
-        );
-        println!("Processing complete. Data written to output.csv");
-    }
+fn print_summary(stats: &PacketStatistics) {
+    println!();
+    println!("packet_count {}", stats.packet_count);
+    println!("tcp_count {}", stats.tcp_count);
+    println!("messages_parsed_count {}", stats.messages_parsed_count);
+    println!(
+        "header_deserialization_failed_count {}",
+        stats.header_deserialization_failed_count
+    );
+    println!(
+        "message_deserialization_failed_count {}",
+        stats.message_deserialization_failed_count
+    );
+    println!("Processing complete. Data written to output.csv");
 }
